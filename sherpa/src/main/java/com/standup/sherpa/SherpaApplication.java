35211ebec16b5e0ffd7c348b830281d3cbd7cb59
Prince Kanoujiya <prince@Princes-MacBook-Air.local>
2025-08-21 23:50:23 +0530

    initial commit

---
+++ sherpa/src/main/java/com/standup/sherpa/SherpaApplication.java
@@ -0,0 +1,13 @@
+package com.standup.sherpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherpaApplication.class, args);
+	}
+
+}
